b08136070442dd6d42f9f3f97188e94111e2433a
RSabiRr <92287092+RSabiRr@users.noreply.github.com>
2021-10-28 04:56:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region task 5
+            //    Console.WriteLine("Ededi daxil edin: ");
+
+
+            //    int n = Convert.ToInt32(Console.ReadLine());
+
+
+            //    say(n);
+
+
+
+
+            //}
+
+            //static void say(int n)
+            //{
+
+            //    string binary = string.Empty;
+
+            //    for (int i = 0; n > 0; i++)
+            //    {
+            //        binary = n % 2 + binary;
+            //        n = n / 2;
+
+
+
+            //    }
+            //    Console.WriteLine($"2-lik Say sistemi: {binary} ");
+
+
+
+
+
+
+
+            //}
+            #endregion
+
+            #region task6
+            //    Console.WriteLine("Ededi daxil edin:  ");
+
+            //    int a = Convert.ToInt32(Console.ReadLine());
+
+
+            //    kub(a);
+
+
+
+            //static void kub(int a)
+            //{
+            //    int cube = 1;
+            //    for (int i = 0; i < 3; i++)
+            //    {
+            //        cube *= a;
+
+
+            //    }
+            //    Console.WriteLine($"Ededin kub'u :  {cube}");
+
+            //}
+            #endregion
+
+            #region task 7
+            //    Console.WriteLine("Ededi daxil edin: ");
+
+            //    int a = Convert.ToInt32(Console.ReadLine());
+
+            //    say(a);
+
+            //}
+
+
+            //static void say(int a)
+            //{
+            //    int b=0;
+            //    while (a!=0)
+            //    {
+            //        a = a / 10;
+
+            //        b++;
+
+
+
+            //    }
+
+            //    Console.WriteLine($"Reqemlerin sayi: {b} ");
+            //}
+            #endregion
+
+            #region task 8
+            //    Console.WriteLine("Ededi daxil edin: ");
+
+            //    int a = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Sayi daxil edin: ");
+
+            //    int b = Convert.ToInt32(Console.ReadLine());
+
+
+
+            //    cem(a, b);
+
+            //}
+
+            //static void cem(int a, int b)
+            //{
+            //    int cem = 0;
+            //    int a2 = a;
+            //    for (int i = 0; i < b; i++)
+            //    {
+            //        cem += a;
+
+            //        a = a * 10 + a2;
+
+
+
+            //    }
+
+            //    Console.WriteLine($"Cem: {cem}");
+            //}
+            #endregion
+
+            #region task 9
+            //    Console.WriteLine("Sayi daxil edin: ");
+
+            //    int num = Convert.ToInt32(Console.ReadLine());
+            //    result(num);
+            //}
+
+            //static void result(int num)
+            //{
+            //    int i, j;
+            //    int max = (num + 1) / 2;
+            //    for (i = 1; i <= max; i++)
+            //    {
+            //        for (j = 1; j <= i; j++)
+            //        {
+            //            Console.Write("*");
+            //        }
+            //        Console.WriteLine();
+            //    }
+            //    for (i = max; i >= 1; i--)
+            //    {
+            //        for (j = 1; j < i; j++)
+            //        {
+            //            Console.Write("*");
+            //        }
+            //        Console.WriteLine();
+            //    }
+            #endregion
+
+            #region task 10
+        //    string text = "Hello, how are you?";
+
+        //    int countSpaces = CountSpaces(text);
+        //    Console.WriteLine("Bosluqlarin sayi: "+countSpaces);
+        //}
+
+        //static int CountSpaces(string text)
+        //{
+        //    int b = 0;
+        //    string text2;
+
+        //    for (int i = 0; i < text.Length; i++)
+        //    {
+        //        text2 = text.Substring(i, 1);
+
+        //        if (text2 == " ")
+        //        {
+        //            b++;
+        //        }
+        //    }
+
+        //    return b;
+            #endregion
+
+        }
+    }
+}
